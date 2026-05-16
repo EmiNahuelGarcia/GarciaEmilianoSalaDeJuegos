70744eeb0dcb022177af854157ede2618406ade3
Emiliano Nahuel Garcia <enahuelgarcia@gmail.com>
2026-05-15 21:43:58 -0300

    ADD: Carcaza del componente login con estilos, añadidos los placeholders para sprint 2 y su funcionamiento

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,9 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  router = inject(Router);
+  showPassword = signal(false);
+  //placeholder para el service del auth de sprint 2
+  error = signal('');
+  loading = signal(false);
+
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.email, Validators.required]),
+    password: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z0-9]*$'), Validators.minLength(8), Validators.maxLength(20)])
+  });
+  
+  async onSubmit() {
+    //placeholder para el submit del login
+}
+
+  togglePasswordVisibility() {
+    this.showPassword.set(!this.showPassword());
+  }
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+}
+
+  
+
+
