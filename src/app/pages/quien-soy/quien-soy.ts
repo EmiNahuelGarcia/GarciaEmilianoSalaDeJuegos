1dab6e402a7ae9b3aa0fb59aaf9a0a7cf0c43779
Emiliano Nahuel Garcia <enahuelgarcia@gmail.com>
2026-05-15 22:05:29 -0300

    ADD : Añadida la funcionalidad a la pagina quien-soy del sprint 1

--- src/app/pages/quien-soy/quien-soy.ts
+++ src/app/pages/quien-soy/quien-soy.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { GitHubService } from '../../services/github';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-quien-soy',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './quien-soy.html',
   styleUrl: './quien-soy.css',
 })
-export class QuienSoy {}
+export class QuienSoy {
+  githubService = inject(GitHubService);
+  usuarioGitHub = this.githubService.usuarioGitHub;
+
+  ngOnInit() {
+    this.githubService.obtenerUsuarioGitHub();
+  }
+
+  formatearFecha(fecha: string): string {
+    if (!fecha) return 'N/A';
+    return new Date(fecha).toLocaleDateString('es-ES', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric'
+    });
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production : false,
+    githubApiUrl : "https://api.github.com/users/",
+    supabaseUrl : "https://qjpssnulxnqxxkosizrz.supabase.co",
+    supabaseKey : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFqcHNzbnVseG5xeHhrb3NpenJ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc5NjMwNjksImV4cCI6MjA5MzUzOTA2OX0.0JArkwuo3vDg4pwFOZTswUcYauEUO7v7i7tjviEAGTE"
+}
